a56887883d02d759a6756b6afa9cf6067a6e12f7
Helison Ortiz Cavalheiro <helissoncavalheiro@gmail.com>
2019-10-04 16:34:35 -0300

    Commit inicial.

---
+++ src/main/java/br/com/helison/models/Aluno.java
@@ -0,0 +1,40 @@
+package br.com.helison.models;
+
+public class Aluno {
+
+    private Long id;
+    private String nome;
+    private String matricula;
+    private String curso;
+
+    public Aluno() {
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
